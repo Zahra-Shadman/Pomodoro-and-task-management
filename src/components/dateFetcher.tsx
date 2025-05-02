62d7f898dec777e4e38a2390f51e65f1f876a915
Zahra-Shadman <zahra.shadmani.work@gmail.com>
2025-05-02 22:49:40 +0330

    define dete picker

---
+++ src/components/dateFetcher.tsx
@@ -0,0 +1,28 @@
+export const formatDate = () => {
+    const date = new Date();
+    const day = date.getDate();
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear();
+  
+
+    let suffix = "TH";
+    if (day === 1 || day === 21 || day === 31) suffix = "ST";
+    else if (day === 2 || day === 22) suffix = "ND";
+    else if (day === 3 || day === 23) suffix = "RD";
+  
+    return `${day}${suffix} ${month} ${year}`; 
+  };
